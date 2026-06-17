51b00a562858fdab39efc9e4fd3032484b0cf073
Felipe <godoyphil9@gmail.com>
2026-06-17 04:33:42 -0300

    feat: add LLM usage reporting domain models

--- app/domain/llm_observability/__init__.py
+++ app/domain/llm_observability/__init__.py
@@ -2,10 +2,18 @@ from app.domain.llm_observability.entities import LLMProviderCallRecord
 from app.domain.llm_observability.enums import LLMProviderCallStatus
 from app.domain.llm_observability.repositories import (
     LLMProviderCallRecordRepository,
+    LLMUsageReportRepository,
+)
+from app.domain.llm_observability.usage_reports import (
+    LLMUsageByModelSummary,
+    LLMUsageSummary,
 )
 
 __all__ = [
     "LLMProviderCallRecord",
     "LLMProviderCallRecordRepository",
     "LLMProviderCallStatus",
+    "LLMUsageByModelSummary",
+    "LLMUsageReportRepository",
+    "LLMUsageSummary",
 ]
